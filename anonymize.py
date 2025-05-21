0a3b95a221a5050fd68a29bcffae4b9f594c864d
Venkatiyyer <venkatiyyer0000@gmail.com>
2025-05-21 16:06:33 +0530

    Update anonymize.py
    old school

--- anonymize.py
+++ anonymize.py
@@ -1,28 +1,7 @@
-from presidio_analyzer import AnalyzerEngine, PatternRecognizer
-from presidio_analyzer.nlp_engine import NlpEngineProvider
+from presidio_analyzer import AnalyzerEngine, PatternRecognizer, EntityRecognizer
 from presidio_anonymizer import AnonymizerEngine
 from presidio_anonymizer.entities import RecognizerResult
 from presidio_anonymizer.entities  import OperatorConfig
-import spacy
-
-# Define the NLP configuration
-configuration = {
-    "nlp_engine_name": "spacy",
-    "models": [{"lang_code": "en", "model_name": "en_core_web_trf"}],
-}
-
-
-# Create the NLP engine
-provider = NlpEngineProvider(nlp_configuration=configuration)
-nlp_engine = provider.create_engine()
-
-
-# Load your transformer model
-def load_trf_model():
-    spacy.cli.download("en_core_web_trf")  # installs only if missing
-    return spacy.load("en_core_web_trf")
-
-nlp = load_trf_model()
 
 
 def analyzer_results(text: str) -> str:
@@ -32,8 +11,9 @@ def analyzer_results(text: str) -> str:
     Returns analyzed text.
     """
     # Initialize the Presidio analyzer engine
-    analyzer = AnalyzerEngine(nlp_engine=nlp_engine, supported_languages=["en"])
+    analyzer = AnalyzerEngine()
     
+
     # Run the anonymizer, returns list of recognizer results
     analyzer_results = analyzer.analyze(text=text,  language='en')
 
@@ -62,6 +42,5 @@ def anonymizer(text: str) -> str:
 
 text_to_anonymize = "His name is Mr. Jones and his phone number is 212-555-5555"
 
-# print(anonymize("My name Rani"))
 print(anonymizer(text_to_anonymize))
 
